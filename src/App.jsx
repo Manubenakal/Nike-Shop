9bc78da187c3bb8c5164e227ad49ebb9bea1300d
Manu M Benakal <manubenakal@gmail.com>
2024-01-24 18:10:43 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => (
+  <main className='relative'>
+    Nav
+    <section className='xl:padding-1 wide:padding-r padding-b'>Hero</section>
+    <section className='padding'>PPP</section>
+    <section className='padding'>Superquality</section>
+    <section className='padding-x py-10'>Services</section>
+    <section className='padding'>Special offer</section>
+    <section className='bg-pale-blue padding'>Customer R</section>
+    <section className='padding-x sm:py-32 py-16 w-full'>subscribe</section>
+    <section className='padding-x padding-t bg-black pb-8'>footer</section>
+  </main>
+);
+export default App;
